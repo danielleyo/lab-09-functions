f360ac430a323576077e84b71a5c78a0b231f869
danielleyo <69905247+danielleyo@users.noreply.github.com>
2020-11-06 20:09:39 -0500

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,8 @@
+def factorial(n):
+    total = 1
+    for i in range(1, n+1):
+        total = total * (i)
+    return total
+
+num = int(input("Please enter a number "))
+print(f'num}! is {factorial(num)}')
